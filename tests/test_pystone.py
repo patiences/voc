eb0df51c4c6530d54ec2d2063bfeb9e318909464
Patience Shyu <patienceshyu@gmail.com>
2018-08-02 15:48:05 +0200

    blah

--- tests/test_pystone.py
+++ tests/test_pystone.py
@@ -8,6 +8,7 @@ class PystoneTest(TranspileTestCase):
 
     def test_pystone(self):
         with open(os.path.join(TESTDIR, "pystone.py")) as javafile:
-            out = self.runAsJava(adjust(javafile.read()), args=["10"])
+            out = self.runAsJava(adjust(javafile.read()), args=["1"])
 
-            self.assertIn("Pystone(1.2) time for 10 passes = ", out)
+            self.assertIn("Pystone(1.2) time for 1 passes = ", out)
+            print(out)
